f9d29342e8e879d25d5fff4d8f47f376cdf677dc
akhildoniki25 <akhildoniki25@gmail.com>
2026-04-13 15:59:05 +0530

    Add files via upload

---
+++ PasswordValidator (1).java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class PasswordValidator {
+    public static boolean isValidPassword(String password) {
+        boolean hasUpperCase = false;
+        boolean hasDigit = false;
+        if (password.length() < 8) {
+            System.out.println("Too short (Minimum 8 characters required)");
+            return false;
+        }
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isUpperCase(ch)) {
+                hasUpperCase = true;
+            }
+
+            if (Character.isDigit(ch)) {
+                hasDigit = true;
+            }
+        }
+        if (!hasUpperCase) {
+            System.out.println("Missing an uppercase letter");
+        }
+
+        if (!hasDigit) {
+            System.out.println("Missing a digit");
+        }
+        return hasUpperCase && hasDigit;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String password;
+        while (true) {
+            System.out.print("Enter your password: ");
+            password = scanner.nextLine();
+
+            if (isValidPassword(password)) {
+                System.out.println("Password is valid!");
+                break;
+            } else {
+                System.out.println("Please try again.\n");
+            }
+        }
+
+        scanner.close();
+    }
+}
