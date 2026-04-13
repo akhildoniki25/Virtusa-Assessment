b630e56508622c77ba52fa1de5a005df64724c74
akhildoniki25 <akhildoniki25@gmail.com>
2026-04-13 15:37:58 +0530

    Add files via upload

---
+++ Digital lib .sql
@@ -0,0 +1,54 @@
+CREATE TABLE Students (
+    StudentID INT PRIMARY KEY,
+    Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    JoinDate DATE,
+    Status VARCHAR(20) DEFAULT 'Active'
+);
+
+CREATE TABLE Books (
+    BookID INT PRIMARY KEY,
+    Title VARCHAR(200) NOT NULL,
+    Author VARCHAR(100),
+    Category VARCHAR(50)
+);
+
+CREATE TABLE IssuedBooks (
+    IssueID INT PRIMARY KEY,
+    StudentID INT,
+    BookID INT,
+    IssueDate DATE NOT NULL,
+    ReturnDate DATE,
+
+    FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
+    FOREIGN KEY (BookID) REFERENCES Books(BookID)
+);
+SELECT 
+    s.StudentID,
+    s.Name,
+    b.Title,
+    ib.IssueDate
+FROM IssuedBooks ib
+JOIN Students s 
+    ON ib.StudentID = s.StudentID
+JOIN Books b 
+    ON ib.BookID = b.BookID
+WHERE 
+    ib.ReturnDate IS NULL
+    AND ib.IssueDate < CURRENT_DATE - INTERVAL '14 days';
+SELECT 
+    b.Category,
+    COUNT(*) AS TotalBorrows
+FROM IssuedBooks ib
+JOIN Books b 
+    ON ib.BookID = b.BookID
+GROUP BY b.Category
+ORDER BY TotalBorrows DESC;
+
+UPDATE Students
+SET Status = 'Inactive'
+WHERE StudentID NOT IN (
+    SELECT DISTINCT StudentID
+    FROM IssuedBooks
+    WHERE IssueDate >= CURRENT_DATE - INTERVAL '3 years'
+);
