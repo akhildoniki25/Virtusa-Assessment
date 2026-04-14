b2cd6a7248bd2edf0ed55ba33a0454fa8912b310
akhildoniki25 <akhildoniki25@gmail.com>
2026-04-14 10:40:54 +0530

    Update Digital lib .sql

--- Digital lib .sql
+++ Digital lib .sql
@@ -1,3 +1,4 @@
+--- it is students table
 CREATE TABLE Students (
     StudentID INT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
@@ -5,14 +6,14 @@ CREATE TABLE Students (
     JoinDate DATE,
     Status VARCHAR(20) DEFAULT 'Active'
 );
-
+-- it is books table 
 CREATE TABLE Books (
     BookID INT PRIMARY KEY,
     Title VARCHAR(200) NOT NULL,
     Author VARCHAR(100),
     Category VARCHAR(50)
 );
-
+-- it is issued books table
 CREATE TABLE IssuedBooks (
     IssueID INT PRIMARY KEY,
     StudentID INT,
@@ -23,6 +24,7 @@ CREATE TABLE IssuedBooks (
     FOREIGN KEY (StudentID) REFERENCES Students(StudentID),
     FOREIGN KEY (BookID) REFERENCES Books(BookID)
 );
+-- students who didn't returned books for more than 14 days
 SELECT 
     s.StudentID,
     s.Name,
@@ -34,6 +36,7 @@ JOIN Books b ON ib.BookID = b.BookID
 WHERE 
     ib.ReturnDate IS NULL
     AND ib.IssueDate < DATE('now', '-14 days');
+-- based on categorys
 SELECT 
     b.Category,
     COUNT(*) AS TotalBorrows
@@ -42,6 +45,7 @@ JOIN Books b
     ON ib.BookID = b.BookID
 GROUP BY b.Category
 ORDER BY TotalBorrows DESC;
+-- no active since 3 years
 
 UPDATE Students
 SET Status = 'Inactive'
